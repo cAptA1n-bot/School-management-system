62bf0b5b2fd1b4fd368c5afa8820fba7bd7384c9
Prince Yadav <prince1bot001@gmail.com>
2026-04-18 00:46:04 +0530

    bug fix

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.json());
 app.use("/api", schoolRoutes);
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
